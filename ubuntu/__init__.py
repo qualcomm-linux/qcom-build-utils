45cf174f7b29a1deae8c196e5a705e011504841b
Neeraj Jetha <njetha@qti.qualcomm.com>
2025-08-14 14:11:03 +0530

    Update __init__.py
    Signed-off-by: Neeraj Jetha <njetha@qti.qualcomm.com>

--- ubuntu/__init__.py
+++ ubuntu/__init__.py
@@ -1,3 +1,4 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
-#
-# SPDX-License-Identifier: BSD-3-Clause-Clear
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Qcom Build Utilities"""
