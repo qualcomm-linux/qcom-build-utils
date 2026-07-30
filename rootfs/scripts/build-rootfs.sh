32d2ed0e7f077e03897fe9d271b1a442ef2e9b9f
Shoudi Li <shoudil@qti.qualcomm.com>
2026-07-30 21:38:13 +0800

    build-rootfs: install package from manifest after linux header
    As part of requirement to integrate adreno packages on server distro,
    ensure linux header package installed before installation of kgsl dkms
    package.
    
    Signed-off-by: Shoudi Li <shoudil@qti.qualcomm.com>

--- rootfs/scripts/build-rootfs.sh
+++ rootfs/scripts/build-rootfs.sh
@@ -631,9 +631,6 @@ adduser --disabled-password --gecos '' qcom
 echo 'qcom:qcom' | chpasswd
 usermod -aG sudo qcom
 
-echo '[CHROOT] Installing manifest packages (if any)...'
-/install_manifest_pkgs.sh || true
-
 echo '[CHROOT] Installing local .deb packages via local APT repository (if any)...'
 if ls /opt/local-debs/*.deb >/dev/null 2>&1; then
     echo '[CHROOT] Setting up local .deb APT repository...'
@@ -650,6 +647,9 @@ if ls /opt/local-debs/*.deb >/dev/null 2>&1; then
 else
     echo '[CHROOT] No local .deb packages found; skipping.'
 fi
+
+echo '[CHROOT] Installing manifest packages (if any)...'
+/install_manifest_pkgs.sh || true
 
 # ==============================================================================
 # Run update-grub after ALL installs (firmware, kernel via dpkg or apt, manifest,
